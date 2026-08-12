397d7823a124ed34ab4da285e8b45dca17f2991d
Uncreative719 <d.millwood719@gmail.com>
2026-08-11 23:55:22 -0500

    Initial creation of Agent with one sentence Chatbot capabilities

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import argparse
+
+def main() -> None:
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if not api_key:
+        raise RuntimeError("API Key not found. OPENROUTER_API_KEY environment variable not set.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [
+     {"role": "user", "content": args.user_prompt},
+    ]
+    response = client.chat.completions.create( messages=messages, model="openrouter/free",)
+    if not response.usage:
+        raise RuntimeError("API reponse appears to be malformed")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Response: {response.choices[0].message.content}")
+
+
+if __name__ == "__main__":
+    main()
